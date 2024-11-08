758b49f6b726201920ab4098123b0f8078cab526
Zhuofan Shi <782530086tony@gmail.com>
2024-11-08 12:57:38 +0800

    initial commit

---
+++ output/train.sh
@@ -0,0 +1,33 @@
+llamafactory-cli train \
+    --stage sft \
+    --do_train True \
+    --model_name_or_path /home/public/data/Model/Qwen1.5-1.8B-Chat \
+    --preprocessing_num_workers 16 \
+    --finetuning_type lora \
+    --template qwen \
+    --flash_attn auto \
+    --dataset_dir data \
+    --dataset MRPC_train_data \
+    --cutoff_len 1024 \
+    --learning_rate 5e-05 \
+    --num_train_epochs 3.0 \
+    --max_samples 100000 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --max_grad_norm 1.0 \
+    --logging_steps 5 \
+    --save_steps 100 \
+    --warmup_steps 0 \
+    --packing False \
+    --report_to none \
+    --output_dir saves/Qwen1.5-1.8B-Chat/lora/test_train1 \
+    --bf16 True \
+    --plot_loss True \
+    --ddp_timeout 180000000 \
+    --optim adamw_torch \
+    --lora_rank 8 \
+    --lora_alpha 16 \
+    --lora_dropout 0 \
+    --lora_target all
+    
